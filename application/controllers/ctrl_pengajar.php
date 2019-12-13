643967c2f73c5532b3e6071d0ac3d341d3db5c05
nuraeni1 <42920080+nuraeni1@users.noreply.github.com>
2019-12-13 22:24:38 +0700

    add files

--- application/controllers/ctrl_pengajar.php
+++ application/controllers/ctrl_pengajar.php
@@ -5,6 +5,8 @@ class ctrl_pengajar extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->model('m_datasiswa1');
+		$this->load->model('m_jurusan');
+    	$this->load->model('m_datasiswa_bimbel');
 		$this->load->helper(array('form', 'url'));
 		if($this->session->userdata('akses') !== '2'){
         	$url=base_url();
@@ -24,4 +26,12 @@ class ctrl_pengajar extends CI_Controller{
     	$data['universitas'] = $this->m_datasiswa1->tampil_data1()->result();
     	$this->load->view('upload/data1',$data);
   }
+  function jurusan(){
+	   	$data['jurusan'] = $this->m_jurusan->tampil_data()->result();
+	    $this->load->view('guru/v_jurusan',$data);
+	}
+	function datasiswabimbel(){
+		$data['tbl_siswa'] = $this->m_datasiswa_bimbel->tampil_data()->result();
+		$this->load->view('guru/v_datasiswa_bimbel',$data);
+	}
 }
